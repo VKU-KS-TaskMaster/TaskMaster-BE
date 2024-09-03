f420881c8315aef7203e5a57b7ed4788777f738f
VietDoan <vietdoan177@gmail.com>
2024-09-03 23:38:36 +0700

    chore: Refactor auth.controller.js and auth.service.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,86 +1,79 @@
 import AuthService from "@/services/auth.service";
 
 const AuthController = {
-    signInDefault: async (req, res, next) => {
-        try {
-            const message = await AuthService.signIn(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    signUpDefault: async (req, res, next) => {
-        try {
-            const message = await AuthService.signUp(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    verifySignUp: async (req, res, next) => {
-        try {
-            const message = await AuthService.verifySignUp(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    signInGoogle: async (req, res, next) => {
-        try {
-            const message = await AuthService.signInGoogle(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    signUpGoogle: async (req, res, next) => {
-        try {
-            const message = await AuthService.signUpGoogle(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    logout: async (req, res, next) => {
-        try {
-            const message = await AuthService.logout(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    forgotPassword: async (req, res, next) => {
-        try {
-            const message = await AuthService.forgotPassword(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-    verifyForgotPassword: async (req, res, next) => {
-        try {
-            const message = await AuthService.verifyForgotPassword(req.body);
-            
-            
-            return res.status(200).json(message)
-        } catch (errors) {
-            next(errors);
-        }
-    },
-}
+  signInDefault: async (req, res, next) => {
+    try {
+      const message = await AuthService.signIn(req.body);
 
-export default AuthController
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  signUpDefault: async (req, res, next) => {
+    try {
+      const message = await AuthService.signUp(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  verifySignUp: async (req, res, next) => {
+    try {
+      const message = await AuthService.verifySignUp(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  signInGoogle: async (req, res, next) => {
+    try {
+      const message = await AuthService.signInGoogle(req.body);
+      if (!message)
+        return res.status(403).json({ message: "Invalid credentials" });
+      return res.status(200).json({ message: message });
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  signUpGoogle: async (req, res, next) => {
+    try {
+      const message = await AuthService.signUpGoogle(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  logout: async (req, res, next) => {
+    try {
+      const message = await AuthService.logout(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  forgotPassword: async (req, res, next) => {
+    try {
+      const message = await AuthService.forgotPassword(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+  verifyForgotPassword: async (req, res, next) => {
+    try {
+      const message = await AuthService.verifyForgotPassword(req.body);
+
+      return res.status(200).json(message);
+    } catch (errors) {
+      next(errors);
+    }
+  },
+};
+
+export default AuthController;
