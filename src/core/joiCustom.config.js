b09e20479b738dd9991594549fda1b5370559f10
dirtylesc <phanxuansy219@gmail.com>
2024-09-13 19:30:45 +0700

    dirtylesc: Update JoiCustom

--- src/core/joiCustom.config.js
+++ src/core/joiCustom.config.js
@@ -1,12 +1,22 @@
 import BaseJoi from "joi"
 import JoiDate from '@joi/date';
 
-const customExtentions = (joi) => ({
-    type: 'stringArray',
-    base: joi.array(),
-    coerce: (value, state, options) => (value.split ? value.split(',') : value)
-});
+const customExtentions = [
+    (joi) => ({
+        type: 'stringArray',
+        base: joi.array(),
+        coerce: (value, state, options) => {
+            if (typeof value === 'string') {
+                return { value: value.split(',') };
+            }
+            if (value === null || value === undefined) {
+                return { value: undefined };
+            }
+            return { value };
+        }
+    })
+]
 
-const JoiCustom = BaseJoi.extend(JoiDate, customExtentions)
+const JoiCustom = BaseJoi.extend(JoiDate, ...customExtentions)
 
 export default JoiCustom
